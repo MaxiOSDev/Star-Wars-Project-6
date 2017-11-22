b3ca409cc2328c03dc40c2b2d03b446b381332fe
Dennis Parussini <thedan84@bitbucket.org>
2017-11-22 09:04:31 +0100

    Added some comments

--- SingleViewAppSwiftTemplate/Networking/JSONDownloader.swift
+++ SingleViewAppSwiftTemplate/Networking/JSONDownloader.swift
@@ -13,11 +13,32 @@ import Foundation
 
 class JSONDownloader {
     
+    /*
+     [REVIEW] The URL could be a little enum
+    
+     enum Endpoint: String {
+     case people
+     case vehicles
+     case starships
+     
+         private var baseURL: String {
+            return "https://swapi.co/api/"
+         }
+     
+         var url: URL {
+            return URL(string: baseURL + self.rawValue)!
+         }
+     }
+    */
     static let base: String = "https://swapi.co/api/"
     static let peopleResource: String = "people/"
     static let vehicleResoure: String = "vehicles/"
     static let starshipResource: String = "starships/"
     static let planetResouce: String = "planets/"
+    
+    
+    
+    //[REVIEW] These don't belong here. The networking layer should only handle downloading the data and passing it along to the next object, all the rest should be handled by a "Manager" struct. More about the "Manager" a little farther down.
     static let planetPages = [Int](min..<max)
     static var stringPlanetPages = planetPages.map {
         String("\($0)/")
@@ -41,6 +62,39 @@ class JSONDownloader {
 
     
 }
+
+/*
+[REVIEW] While this works, it doesn't really make sense to let the networking layer handle all the logic.
+ I would create a fetchEndpoint() function which takes the Endpoint enum as a parameter and then just passes the data to the object asking for it.
+ 
+ static func fetchEndpoint(endpoint: Endpoint, completion: @escaping (Data) -> Void) {
+    let url = endpoint.url
+    let session = URLSession.shared
+    let task = session.dataTask(with: url) { (data, response, error) in
+        if let data = data {
+            completion(data)
+        } else if let error = error {
+            //handle the error or pass it along in the completion block
+        }
+    }
+ }
+ 
+ 
+ I would then create Manager structs for the various types (People, Vehicle and Starship), create methods to fetch and parse the JSON and let this struct pass the JSON to the next object which needs it.
+ For example:
+ 
+ struct PeopleManager {
+ 
+    //This can also use a completion block, or just return the array of people. Whatever you like best.
+    static func fetchPeople() -> [People] {
+        JSONDownloader.fetchEndpoint(endpoint: .people) { data in
+            //decode the data to JSON and return the array of people
+        }
+    }
+ }
+
+*/
+
 
 // Character JSON
 extension JSONDownloader {
